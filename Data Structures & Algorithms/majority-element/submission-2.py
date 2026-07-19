536e806f6c950856ceb1adc0575ca3de2980cc1c
Luis Herrera <luhego@users.noreply.github.com>
2026-07-19 11:15:15 -0700

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.py
@@ -0,0 +1,23 @@
+"""
+Time complexity: O(N)
+Space complexity: O(1)
+Time: 3min
+"""
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        candidate = nums[0]
+        votes = 1
+
+        for i in range(1, len(nums)):
+            num = nums[i]
+            if num == candidate:
+                votes += 1
+            else:
+                votes -= 1
+
+                if votes == 0:
+                    candidate = num
+                    votes = 1
+        
+        return candidate
+      

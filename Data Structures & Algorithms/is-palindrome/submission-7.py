eb48fd24b8a0953dbd4dd3b76c10c49ea8a3f70e
Luis Herrera <luhego@users.noreply.github.com>
2026-07-20 22:08:05 -0700

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.py
@@ -0,0 +1,22 @@
+"""
+Time complexity: O(N)
+Space complexity: O(1)
+Time: 2min
+"""
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            if not s[left].isalnum():
+                left += 1
+            elif not s[right].isalnum():
+                right -= 1
+            elif s[left].lower() == s[right].lower():
+                left += 1
+                right -= 1
+            else:
+                return False
+        
+        return True

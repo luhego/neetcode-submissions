85c830bb188b97d5a7ae5a9e62dcc810cc05828a
Luis Herrera <luhego@users.noreply.github.com>
2026-07-20 22:11:19 -0700

    Add: valid-palindrome-ii - submission-2

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-2.py
@@ -0,0 +1,25 @@
+"""
+Time complexity: O(N)
+Space complexity: O(1)
+Time: 3min
+"""
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        def isPalindrome(left, right):
+            while left < right:
+                if s[left] != s[right]:
+                    return False
+                left += 1
+                right -= 1
+            return True
+
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            if s[left] != s[right]:
+                return isPalindrome(left + 1, right) or isPalindrome(left, right - 1)
+            left += 1
+            right -= 1
+        
+        return True
+      

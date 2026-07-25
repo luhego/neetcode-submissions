e7aa00237ea3e4e21ee6f571674bebac57a35ce0
Luis Herrera <luhego@users.noreply.github.com>
2026-07-24 23:39:46 -0700

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.py
@@ -0,0 +1,20 @@
+"""
+Time complexity: O(logN)
+Space complexity: O(1)
+Time: 2min
+"""
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low, high = 0, len(nums) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        
+        return -1

cca12b5399b5860642d0c110d939e61af013dd32
Luis Herrera <luhego@users.noreply.github.com>
2026-07-25 09:57:12 -0700

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.py
@@ -0,0 +1,31 @@
+"""
+Time complexity: O(NlogN)
+Space complexity: O(1)
+Time: 12min
+"""
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def canEatBananasWithinH(k):
+            total_hours = 0
+            for pile in piles:
+                total_hours += (pile // k)
+                if pile % k > 0:
+                    total_hours += 1
+                if total_hours > h:
+                    return False
+            return True
+
+        low = 1
+        high = max(piles)
+
+        answer = 0
+        while low <= high:
+            mid = (low + high) // 2
+
+            if canEatBananasWithinH(mid):
+                answer = mid
+                high = mid - 1
+            else:
+                low = mid + 1
+        
+        return answer

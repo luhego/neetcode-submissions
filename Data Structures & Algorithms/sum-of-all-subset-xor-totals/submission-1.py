a0b47655f50bd44e467053845357436a91532074
Luis Herrera <luhego@users.noreply.github.com>
2026-08-01 18:08:53 -0700

    Add: sum-of-all-subset-xor-totals - submission-1

---
+++ Data Structures & Algorithms/sum-of-all-subset-xor-totals/submission-1.py
@@ -0,0 +1,22 @@
+"""
+Time complexity: O(2^N)
+Space complexity: O(N)
+Time: 18min
+"""
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+        def subsets(i, xor_total):
+            if i == n:
+                self.total_sum += xor_total
+                return
+
+            # Take the current element
+            subsets(i + 1, xor_total ^ nums[i])
+
+            # Skip the current element
+            subsets(i + 1, xor_total)
+
+        self.total_sum = 0
+        n = len(nums)
+        subsets(0, 0)        
+        return self.total_sum
